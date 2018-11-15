240ae84b5197bf478cb94cb089c45baca4956d49
nekoinverse <35851828+nekoinverse@users.noreply.github.com>
2018-11-15 16:59:47 +0100

    created simple censor

--- src/SimpleCensor.php
+++ src/SimpleCensor.php
@@ -6,5 +6,18 @@ class SimpleCensor extends AbstractCensor
 {
     public function __invoke(array $censoredWords, string $text) : string
     {
+        foreach ($censoredWords as $censoredWord) {
+            $text = $this->censorWord($censoredWord, $text);
+        }
+       return $text;
+    }
+
+    protected function censorWord (string $word, string $text) : string
+    {
+        $asterisks  = str_repeat("*", strlen($word));
+        $pattern    = '/\b' .$word. '\b/i';
+
+        $replaced_text = preg_replace($pattern, $asterisks, $text);
+        return $replaced_text;
     }
 }
